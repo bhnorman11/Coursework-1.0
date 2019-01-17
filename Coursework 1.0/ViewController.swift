258e4c034c511a0282a6bb760d2fbe1fab57d921
Ben Norman <Ben@users-macbook-air-2.local>
2019-01-17 18:30:19 +0000

    Initial Commit

---
+++ Coursework 1.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coursework 1.0
+//
+//  Created by Ben Norman on 17/01/2019.
+//  Copyright © 2019 Ben Norman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Coursework 1.0Tests/Coursework_1_0Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Coursework_1_0Tests.swift
+//  Coursework 1.0Tests
+//
+//  Created by Ben Norman on 17/01/2019.
+//  Copyright © 2019 Ben Norman. All rights reserved.
+//
+
+import XCTest
+@testable import Coursework_1_0
+
+class Coursework_1_0Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
