f2c0f0e8cdbc0063000c811a73ee31ed6885051a
Ben Norman <Ben@users-air-2.domain_not_set.invalid>
2019-01-26 23:35:34 +0000

    Successfully registered and logged in with a dummy user "bhnorman11@gmail.com", added secure password entry and error message when logging in with an incorrect email/password

--- Coursework 1.0/LoginViewController.swift
+++ Coursework 1.0/LoginViewController.swift
@@ -13,18 +13,21 @@ class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        Error.isHidden = true
 
 
     }
     
     @IBOutlet weak var Email: UITextField!
     @IBOutlet weak var Password: UITextField!
+    @IBOutlet weak var Error: UILabel!
     
 
     @IBAction func Login(_ sender: Any) {
         Auth.auth().signIn(withEmail: (Email.text!), password: (Password.text!)) { (user, error) in
             if error != nil{
                 print(error!)
+                self.Error.isHidden = false
             }else{
                 print("Login successful")
                 self.performSegue(withIdentifier: "Login", sender: self)
