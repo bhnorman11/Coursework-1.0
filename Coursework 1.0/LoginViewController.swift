f8065f7c3e125f19d1c388cf8ac41fba936d5076
Ben Norman <Ben@users-macbook-air-2.local>
2019-02-27 23:32:53 +0000

    Successfully managed to read the status of whether the user is a student or teacher off of Firestore, and then go to the appropriate main page.

--- Coursework 1.0/LoginViewController.swift
+++ Coursework 1.0/LoginViewController.swift
@@ -9,12 +9,13 @@
 import UIKit
 import FirebaseAuth
 import FirebaseFirestore
+import Firebase
 
 class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        Error.isHidden = true
 
 
     }
@@ -23,17 +24,36 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var Password: UITextField!
     @IBOutlet weak var Error: UILabel!
     
-
+    let db = Firestore.firestore()
+    let user = Auth.auth().currentUser
+    var studentType = false
+    
+    
     @IBAction func Login(_ sender: Any) {
         Auth.auth().signIn(withEmail: (Email.text!), password: (Password.text!)) { (user, error) in
             if error != nil{
                 print(error!)
                 self.Error.isHidden = false
             }else{
-                print("Login successful")
-                self.performSegue(withIdentifier: "Login", sender: self)
+                let email = self.Email.text!
+                let docRef = self.db.collection("Users").document(email)
+                docRef.getDocument(source: .cache) { (document, error) in
+                    if let document = document{
+                        self.studentType = (document.get("Student") as! Bool)
+                        if self.studentType == true {
+                            self.performSegue(withIdentifier: "StudentMain", sender: self)
+                        }
+                        else if self.studentType == false {
+                            self.performSegue(withIdentifier: "TeacherMain", sender: self)
+                        }
+                    }
+                    else{
+                        print("Document does not exist in firestore")
+                    }
+                }
+                }
+                
             }
-        }
     }
     
     @IBAction func Back(_ sender: Any) {
