fccdbacfcb82bbcbb73346daae46f414f52ff81b
Ben Norman <Ben@users-macbook-air-2.local>
2019-03-07 17:50:12 +0000

    Managed to add students to a table view cell in create new class. Successfully written the new class to firestore under the teachers page. Error: automatically segues to the home page when creating the class.

--- Coursework 1.0/CreateNewClassViewController.swift
+++ Coursework 1.0/CreateNewClassViewController.swift
@@ -7,8 +7,26 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseFirestore
 
-class CreateNewClassViewController: UIViewController {
+class CreateNewClassViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return studentArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "studentCell", for: indexPath)
+        cell.textLabel?.text = studentArray[indexPath.row]
+        return cell
+        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,15 +34,50 @@ class CreateNewClassViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBOutlet weak var StudentEmail: UITextField!
     @IBOutlet weak var Subject: UITextField!
     @IBOutlet weak var Block: UITextField!
     @IBOutlet weak var Set: UITextField!
-    @IBOutlet weak var StudentName: UITextField!
+    @IBOutlet weak var studentTable: UITableView!
+    var studentArray = [String]()
     
     @IBAction func AddStudent(_ sender: Any) {
+        studentArray.append(StudentEmail.text!)
+        self.studentTable.reloadData()
+        StudentEmail.text = ""
     }
     
+    let db = Firestore.firestore()
+    let user = Auth.auth().currentUser
+    
     @IBAction func CreateClass(_ sender: Any) {
+        let email = user?.email
+        db.collection("Teachers").document(email!).collection("Classes").document(Set.text!).setData([
+            "Block": Block.text!,
+            "Subject": Subject.text!
+        ])
+        { err in
+            if let err = err {
+                print("Error writing document: \(err)")
+            } else {
+                print("Document successfully written!")
+                
+            }
+        }
+        for i in 0...studentArray.count - 1 {
+            db.collection("Teachers").document(email!).collection("Classes").document(Set.text!).collection("Students").document(studentArray[i]).setData([
+                "Email": studentArray[i],
+            ])
+            { err in
+                if let err = err {
+                    print("Error writing document: \(err)")
+                } else {
+                    print("Document successfully written!")
+                    
+                }
+            }
+        }
+        
     }
     
 }
