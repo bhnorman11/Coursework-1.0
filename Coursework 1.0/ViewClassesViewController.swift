b2460472ce6e5ef8a6a4651dd16559630848f315
Ben Norman <Ben@users-macbook-air-2.local>
2019-03-08 21:54:59 +0000

    Designed layout for viewing current class, including a picker view and a table view. Created new ViewClassesViewController too.

---
+++ Coursework 1.0/ViewClassesViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewClassesViewController.swift
+//  Coursework 1.0
+//
+//  Created by Ben Norman on 08/03/2019.
+//  Copyright © 2019 Ben Norman. All rights reserved.
+//
+
+import UIKit
+
+class ViewClassesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBOutlet weak var SetPicker: UIPickerView!
+    @IBOutlet weak var SubjectLabel: UILabel!
+    @IBOutlet weak var SubjectText: UILabel!
+    @IBOutlet weak var BlockLabel: UILabel!
+    @IBOutlet weak var BlockTest: UILabel!
+    
+
+}
