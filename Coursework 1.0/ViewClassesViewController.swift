5a9413685f0ac639236b68630042afae89c88ba8
Ben Norman <Ben@users-macbook-air-2.local>
2019-03-08 22:02:32 +0000

    Started writing code to select a class from firestore.

--- Coursework 1.0/ViewClassesViewController.swift
+++ Coursework 1.0/ViewClassesViewController.swift
@@ -8,12 +8,35 @@
 
 import UIKit
 
-class ViewClassesViewController: UIViewController {
+class ViewClassesViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        <#code#>
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        <#code#>
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        <#code#>
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        SubjectLabel.isHidden = true
+        SubjectText.isHidden = true
+        BlockLabel.isHidden = true
+        BlockTest.isHidden = true
+        AddEmailLabel.isHidden = true
+        AddEmailText.isHidden = true
+        AddEmailButton.isHidden = true
+        StudentTable.isHidden = true
     }
     
 
@@ -22,6 +45,10 @@ class ViewClassesViewController: UIViewController {
     @IBOutlet weak var SubjectText: UILabel!
     @IBOutlet weak var BlockLabel: UILabel!
     @IBOutlet weak var BlockTest: UILabel!
+    @IBOutlet weak var StudentTable: UITableView!
+    @IBOutlet weak var AddEmailLabel: UILabel!
+    @IBOutlet weak var AddEmailText: UITextField!
+    @IBOutlet weak var AddEmailButton: UIButton!
     
 
 }
