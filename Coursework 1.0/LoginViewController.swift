5bd1607e9bca33391e43842645854ffb8ebe9418
Ben Norman <Ben@users-macbook-air-4.local>
2019-03-26 14:14:53 +0000

    Added some validation to the login page that checks for @ symbols, full stops and whether the spaces are blank

--- Coursework 1.0/LoginViewController.swift
+++ Coursework 1.0/LoginViewController.swift
@@ -29,32 +29,68 @@ class LoginViewController: UIViewController {
     let user = Auth.auth().currentUser
     var studentType = false
     
+    func checkFieldsEmpty() -> Bool{
+        if (Email.text == "" ) || (Password.text == "") {
+            Error.text = "Please fill in all spaces."
+            Error.isHidden = false
+            return false
+        }
+        else {
+            return true
+        }
+    }
+    
+    func checkEmail() -> Bool {
+        var atSymbol = false
+        var fullStop = false
+        for character in Email.text!.characters {
+            if character == "@" {
+                atSymbol = true
+            }
+            else if character == "." {
+                fullStop = true
+            }
+        }
+        if (atSymbol == true) && (fullStop == true) {
+            return true
+        }
+        else {
+            Error.text = "Please have an @ and full stop in your email."
+            Error.isHidden = false
+            return false
+        }
+    }
+    
+    
     
     @IBAction func Login(_ sender: Any) {
-        Auth.auth().signIn(withEmail: (Email.text!), password: (Password.text!)) { (user, error) in
-            if error != nil{
-                print(error!)
-                self.Error.isHidden = false
-            }else{
-                let email = self.Email.text!
-                let docRef = self.db.collection("Users").document(email)
-                docRef.getDocument(source: .cache) { (document, error) in
-                    if let document = document{
-                        self.studentType = (document.get("Student") as! Bool)
-                        if self.studentType == true {
-                            self.performSegue(withIdentifier: "StudentMain", sender: self)
+        if (checkFieldsEmpty() == true) && (checkEmail() == true){
+            Auth.auth().signIn(withEmail: (Email.text!), password: (Password.text!)) { (user, error) in
+                if error != nil{
+                    print(error!)
+                    self.Error.isHidden = false
+                }else{
+                    let email = self.Email.text!
+                    let docRef = self.db.collection("Users").document(email)
+                    docRef.getDocument(source: .cache) { (document, error) in
+                        if let document = document{
+                            self.studentType = (document.get("Student") as! Bool)
+                            if self.studentType == true {
+                                self.performSegue(withIdentifier: "StudentMain", sender: self)
+                            }
+                            else if self.studentType == false {
+                                self.performSegue(withIdentifier: "TeacherMain", sender: self)
+                            }
                         }
-                        else if self.studentType == false {
-                            self.performSegue(withIdentifier: "TeacherMain", sender: self)
+                        else{
+                            print("Document does not exist in firestore")
                         }
                     }
-                    else{
-                        print("Document does not exist in firestore")
-                    }
-                }
                 }
                 
             }
+        }
+        
     }
     
     
