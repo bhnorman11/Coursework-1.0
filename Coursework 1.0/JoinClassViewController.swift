ebd84965e069f0ad5798a61c62c06ffa79507275
Ben Norman <Ben@users-macbook-air-4.local>
2019-11-10 15:34:15 +0000

    Created new join class view from the student home page as well as a view controller. Need to make a connection to Firestore as well as validate the code entry.

---
+++ Coursework 1.0/JoinClassViewController.swift
@@ -0,0 +1,25 @@
+//
+//  JoinClassViewController.swift
+//  Coursework 1.0
+//
+//  Created by Ben Norman on 10/11/2019.
+//  Copyright © 2019 Ben Norman. All rights reserved.
+//
+
+import UIKit
+
+class JoinClassViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var Code: UITextField!
+    
+    @IBAction func Continue(_ sender: Any) {
+    }
+    
+
+}
